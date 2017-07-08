2e691dcd0304272409e118d81a4fea2cdc17ac54
Julio Olivera <julio.olvr@gmail.com>
2017-07-08 18:53:17 -0300

    Prettier

--- src/lib/excerciseGenerator.test.js
+++ src/lib/excerciseGenerator.test.js
@@ -1,100 +1,119 @@
-import { expect } from 'chai'
+import { expect } from "chai";
 import createExercise, {
   randomFromArray,
   distanceInArray,
   distance,
-  C_FLAT, C, C_SHARP, D_FLAT, D, D_SHARP, E_FLAT, E, E_SHARP, F_FLAT, F, F_SHARP,
-  G_FLAT, G, G_SHARP, A_FLAT, A, A_SHARP, B_FLAT, B, B_SHARP
-} from './exerciseGenerator'
+  C_FLAT,
+  C,
+  C_SHARP,
+  D_FLAT,
+  D,
+  D_SHARP,
+  E_FLAT,
+  E,
+  E_SHARP,
+  F_FLAT,
+  F,
+  F_SHARP,
+  G_FLAT,
+  G,
+  G_SHARP,
+  A_FLAT,
+  A,
+  A_SHARP,
+  B_FLAT,
+  B,
+  B_SHARP
+} from "./exerciseGenerator";
 
-describe('randomFromArray', () => {
-  it('returns undefined for an empty array', () => {
-    expect(randomFromArray([])).to.be.undefined
-  })
+describe("randomFromArray", () => {
+  it("returns undefined for an empty array", () => {
+    expect(randomFromArray([])).to.be.undefined;
+  });
 
-  it('returns the single element of a one-element array', () => {
-    const element = 1
-    expect(randomFromArray([element])).to.equal(element)
-  })
+  it("returns the single element of a one-element array", () => {
+    const element = 1;
+    expect(randomFromArray([element])).to.equal(element);
+  });
 
-  it('always returns an element from the array', () => {
-    const arr = [1, 2, 3, 4, 5]
+  it("always returns an element from the array", () => {
+    const arr = [1, 2, 3, 4, 5];
 
     for (let i = 0; i < 10; i++) {
-      const randomElement = randomFromArray(arr)
-      expect(arr).to.include(randomElement)
+      const randomElement = randomFromArray(arr);
+      expect(arr).to.include(randomElement);
     }
-  })
-})
+  });
+});
 
-describe('distanceInArray', () => {
-  it('counts the amount in between if the second element is after the first', () => {
-    const arr = ['A', 'B', 'C', 'D', 'E']
-    expect(distanceInArray(arr, 'B', 'D')).to.equal(2)
-  })
+describe("distanceInArray", () => {
+  it("counts the amount in between if the second element is after the first", () => {
+    const arr = ["A", "B", "C", "D", "E"];
+    expect(distanceInArray(arr, "B", "D")).to.equal(2);
+  });
 
-  it('counts all the way around if the second element is before the first', () => {
-    const arr = ['A', 'B', 'C', 'D', 'E']
-    expect(distanceInArray(arr, 'D', 'B')).to.equal(3)
-  })
-})
+  it("counts all the way around if the second element is before the first", () => {
+    const arr = ["A", "B", "C", "D", "E"];
+    expect(distanceInArray(arr, "D", "B")).to.equal(3);
+  });
+});
 
-describe('distance', () => {
-  it('is m2 for one semitone', () => {
-    expect(distance(C, D_FLAT).name).to.equal('m2')
-  })
+describe("distance", () => {
+  it("is m2 for one semitone", () => {
+    expect(distance(C, D_FLAT).name).to.equal("m2");
+  });
 
-  it('is M2 for two semitones', () => {
-    expect(distance(C, D).name).to.equal('M2')
-  })
+  it("is M2 for two semitones", () => {
+    expect(distance(C, D).name).to.equal("M2");
+  });
 
-  it('is m3 for three semitones', () => {
-    expect(distance(C, E_FLAT).name).to.equal('m3')
-  })
+  it("is m3 for three semitones", () => {
+    expect(distance(C, E_FLAT).name).to.equal("m3");
+  });
 
-  it('is M3 for four semitones', () => {
-    expect(distance(C, E).name).to.equal('M3')
-  })
+  it("is M3 for four semitones", () => {
+    expect(distance(C, E).name).to.equal("M3");
+  });
 
-  it('is P4 for five semitones', () => {
-    expect(distance(C, F).name).to.equal('P4')
-  })
+  it("is P4 for five semitones", () => {
+    expect(distance(C, F).name).to.equal("P4");
+  });
 
-  it('is A4 for six semitones if the notes are 4 letters apart', () => {
-    expect(distance(C, F_SHARP).name).to.equal('A4')
-  })
+  it("is A4 for six semitones if the notes are 4 letters apart", () => {
+    expect(distance(C, F_SHARP).name).to.equal("A4");
+  });
 
-  it('is d5 for six semitones if the notes are 5 letters apart', () => {
-    expect(distance(C, G_FLAT).name).to.equal('d5')
-  })
+  it("is d5 for six semitones if the notes are 5 letters apart", () => {
+    expect(distance(C, G_FLAT).name).to.equal("d5");
+  });
 
-  it('is P5 for 7 semitones', () => {
-    expect(distance(C, G).name).to.equal('P5')
-  })
+  it("is P5 for 7 semitones", () => {
+    expect(distance(C, G).name).to.equal("P5");
+  });
 
-  it('is m6 for 8 semitones', () => {
-    expect(distance(C, A_FLAT).name).to.equal('m6')
-  })
+  it("is m6 for 8 semitones", () => {
+    expect(distance(C, A_FLAT).name).to.equal("m6");
+  });
 
-  it('is M6 for 9 semitones', () => {
-    expect(distance(C, A).name).to.equal('M6')
-  })
+  it("is M6 for 9 semitones", () => {
+    expect(distance(C, A).name).to.equal("M6");
+  });
 
-  it('is m7 for 10 semitones', () => {
-    expect(distance(C, B_FLAT).name).to.equal('m7')
-  })
+  it("is m7 for 10 semitones", () => {
+    expect(distance(C, B_FLAT).name).to.equal("m7");
+  });
 
-  it('is M7 for 11 semitones', () => {
-    expect(distance(C, B).name).to.equal('M7')
-  })
+  it("is M7 for 11 semitones", () => {
+    expect(distance(C, B).name).to.equal("M7");
+  });
 
-  it('throws an error for > 11 semitones', () => {
-    expect(() => distance(C, B_SHARP)).to.throw()
-  })
-})
+  it("throws an error for > 11 semitones", () => {
+    expect(() => distance(C, B_SHARP)).to.throw();
+  });
+});
 
-describe('excerciseGenerator', () => {
-  it('works', () => {
-    expect(createExercise).not.to.be.undefined
-  })
-})
+describe("excerciseGenerator", () => {
+  it("works", () => {
+    expect(createExercise).not.to.be.undefined;
+  });
+});
--- src/lib/exerciseGenerator.js
+++ src/lib/exerciseGenerator.js
@@ -1,117 +1,117 @@
 // @flow
 
-type NoteLetter = 'C' | 'D' | 'E' | 'F' | 'G' | 'A' | 'B'
-type NoteModifier = 'Sharp' | 'Flat'
+type NoteLetter = "C" | "D" | "E" | "F" | "G" | "A" | "B";
+type NoteModifier = "Sharp" | "Flat";
 
-const SHARP: NoteModifier = 'Sharp'
-const FLAT: NoteModifier = 'Flat'
+const SHARP: NoteModifier = "Sharp";
+const FLAT: NoteModifier = "Flat";
 
 type Note = {
   letter: NoteLetter,
   modifier?: NoteModifier
-}
+};
 
-type NoteGroup = [Note]
+type NoteGroup = [Note];
 
 export const C_FLAT = {
-  letter: 'C',
+  letter: "C",
   modifier: FLAT
-}
+};
 
 export const C = {
-  letter: 'C'
-}
+  letter: "C"
+};
 
 export const C_SHARP = {
-  letter: 'C',
+  letter: "C",
   modifier: SHARP
-}
+};
 
 export const D_FLAT = {
-  letter: 'D',
+  letter: "D",
   modifier: FLAT
-}
+};
 
 export const D = {
-  letter: 'D'
-}
+  letter: "D"
+};
 
 export const D_SHARP = {
-  letter: 'D',
+  letter: "D",
   modifier: SHARP
-}
+};
 
 export const E_FLAT = {
-  letter: 'E',
+  letter: "E",
   modifier: FLAT
-}
+};
 
 export const E = {
-  letter: 'E'
-}
+  letter: "E"
+};
 
 export const E_SHARP = {
-  letter: 'E',
+  letter: "E",
   modifier: SHARP
-}
+};
 
 export const F_FLAT = {
-  letter: 'F',
+  letter: "F",
   modifier: FLAT
-}
+};
 
 export const F = {
-  letter: 'F'
-}
+  letter: "F"
+};
 
 export const F_SHARP = {
-  letter: 'F',
+  letter: "F",
   modifier: SHARP
-}
+};
 
 export const G_FLAT = {
-  letter: 'G',
+  letter: "G",
   modifier: FLAT
-}
+};
 
 export const G = {
-  letter: 'G'
-}
+  letter: "G"
+};
 
 export const G_SHARP = {
-  letter: 'G',
+  letter: "G",
   modifier: SHARP
-}
+};
 
 export const A_FLAT = {
-  letter: 'A',
+  letter: "A",
   modifier: FLAT
-}
+};
 
 export const A = {
-  letter: 'A'
-}
+  letter: "A"
+};
 
 export const A_SHARP = {
-  letter: 'A',
+  letter: "A",
   modifier: SHARP
-}
+};
 
 export const B_FLAT = {
-  letter: 'B',
+  letter: "B",
   modifier: FLAT
-}
+};
 
 export const B = {
-  letter: 'B'
-}
+  letter: "B"
+};
 
 export const B_SHARP = {
-  letter: 'B',
+  letter: "B",
   modifier: SHARP
-}
+};
 
-const NOTE_LETTERS: [NoteLetter] = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
+const NOTE_LETTERS: [NoteLetter] = ["C", "D", "E", "F", "G", "A", "B"];
 
 const NOTE_GROUPS: [NoteGroup] = [
   [B_SHARP, C],
@@ -126,103 +126,140 @@ const NOTE_GROUPS: [NoteGroup] = [
   [A],
   [A_SHARP, B_FLAT],
   [B, C_FLAT]
-]
+];
 
 type Exercise = {
   from: Note,
   to: Note,
   distance: Distance
-}
+};
 
 type Distance = {
   name: string,
   semitones: number
-}
+};
 
-type RelativeInterval = number
+type RelativeInterval = number;
 
 export function randomFromArray<T>(arr: [T]): T {
-  const index = Math.floor(Math.random() * arr.length)
-  return arr[index]
+  const index = Math.floor(Math.random() * arr.length);
+  return arr[index];
 }
 
 export function distanceInArray<T>(arr: [T], a: T, b: T): number {
-  const indexA = arr.indexOf(a)
-  const indexB = arr.indexOf(b)
+  const indexA = arr.indexOf(a);
+  const indexB = arr.indexOf(b);
 
   if (indexB > indexA) {
-    return indexB - indexA
+    return indexB - indexA;
   } else {
-    return arr.length - indexA + indexB
+    return arr.length - indexA + indexB;
   }
 }
 
-const distanceBetweenLetters = distanceInArray.bind(null, NOTE_LETTERS)
+const distanceBetweenLetters = distanceInArray.bind(null, NOTE_LETTERS);
 
 function getRandomNoteGroup(options: { except?: Note } = {}): NoteGroup {
-  const { except } = options
+  const { except } = options;
   return randomFromArray(
-    except ?
-    NOTE_GROUPS.filter(noteGroup => noteGroup.filter(note => note.letter !== except.letter).length) :
-    NOTE_GROUPS
-  )
+    except
+      ? NOTE_GROUPS.filter(
+          noteGroup =>
+            noteGroup.filter(note => note.letter !== except.letter).length
+        )
+      : NOTE_GROUPS
+  );
 }
 
 export function distance(from: Note, to: Note): Distance {
-  const semitones = semitonesBetweenNotes(from, to)
+  const semitones = semitonesBetweenNotes(from, to);
   let realDistance;
 
   switch (semitones) {
-    case 1: realDistance = 'm2'; break
-    case 2: realDistance = 'M2'; break
-    case 3: realDistance = 'm3'; break
-    case 4: realDistance = 'M3'; break
-    case 5: realDistance = 'P4'; break
-    case 7: realDistance = 'P5'; break
-    case 8: realDistance = 'm6'; break
-    case 9: realDistance = 'M6'; break
-    case 10: realDistance = 'm7'; break
-    case 11: realDistance = 'M7'; break
+    case 1:
+      realDistance = "m2";
+      break;
+    case 2:
+      realDistance = "M2";
+      break;
+    case 3:
+      realDistance = "m3";
+      break;
+    case 4:
+      realDistance = "M3";
+      break;
+    case 5:
+      realDistance = "P4";
+      break;
+    case 7:
+      realDistance = "P5";
+      break;
+    case 8:
+      realDistance = "m6";
+      break;
+    case 9:
+      realDistance = "M6";
+      break;
+    case 10:
+      realDistance = "m7";
+      break;
+    case 11:
+      realDistance = "M7";
+      break;
     case 6:
-      const relativeInterval = distanceBetweenLetters(from.letter, to.letter)
-      realDistance = relativeInterval === 3 ? 'A4' : 'd5'
-      break
+      const relativeInterval = distanceBetweenLetters(from.letter, to.letter);
+      realDistance = relativeInterval === 3 ? "A4" : "d5";
+      break;
   }
 
   if (!realDistance) {
-    throw new Error(`Distance between notes sould be between 1 and 11, was ${semitones}`)
+    throw new Error(
+      `Distance between notes sould be between 1 and 11, was ${semitones}`
+    );
   }
 
   return {
     name: realDistance,
     semitones
-  }
+  };
 }
 
 function semitonesBetweenNotes(fromNote: Note, toNote: Note): number {
-  const toGroup = NOTE_GROUPS.find(group => group.includes(toNote))
-  return semitonesFromNoteToGroup(fromNote, toGroup)
+  const toGroup = NOTE_GROUPS.find(group => group.includes(toNote));
+  return semitonesFromNoteToGroup(fromNote, toGroup);
 }
 
 function semitonesFromNoteToGroup(fromNote: Note, toGroup: NoteGroup): number {
-  const fromGroup = NOTE_GROUPS.find(group => group.includes(fromNote))
-  return distanceInArray(NOTE_GROUPS, fromGroup, toGroup)
+  const fromGroup = NOTE_GROUPS.find(group => group.includes(fromNote));
+  return distanceInArray(NOTE_GROUPS, fromGroup, toGroup);
 }
 
 function relativeIntervalsForSemitones(semitones: number): [RelativeInterval] {
   switch (semitones) {
-    case 1: return [2]
-    case 2: return [2]
-    case 3: return [3]
-    case 4: return [3]
-    case 5: return [4]
-    case 6: return [4, 5]
-    case 7: return [5]
-    case 8: return [6]
-    case 9: return [6]
-    case 10: return [7]
-    case 11: return [7]
-    default: throw new Error(`Semitones must be between 1 and 11, were ${semitones}`)
+    case 1:
+      return [2];
+    case 2:
+      return [2];
+    case 3:
+      return [3];
+    case 4:
+      return [3];
+    case 5:
+      return [4];
+    case 6:
+      return [4, 5];
+    case 7:
+      return [5];
+    case 8:
+      return [6];
+    case 9:
+      return [6];
+    case 10:
+      return [7];
+    case 11:
+      return [7];
+    default:
+      throw new Error(`Semitones must be between 1 and 11, were ${semitones}`);
   }
 }
 
@@ -230,47 +267,50 @@ function relativeLetterForInterval(
   fromLetter: NoteLetter,
   interval: RelativeInterval
 ): NoteLetter {
-  const baseIndex = NOTE_LETTERS.indexOf(fromLetter)
-  let newIndex = baseIndex + interval - 1
+  const baseIndex = NOTE_LETTERS.indexOf(fromLetter);
+  let newIndex = baseIndex + interval - 1;
 
   if (newIndex >= NOTE_LETTERS.length) {
-    newIndex -= NOTE_LETTERS.length
+    newIndex -= NOTE_LETTERS.length;
   }
 
-  return NOTE_LETTERS[newIndex]
+  return NOTE_LETTERS[newIndex];
 }
 
 function validNoteFromGroup(baseNote: Note, destinationGroup: NoteGroup): Note {
-  const semitones = semitonesFromNoteToGroup(baseNote, destinationGroup)
-  const relativeIntervals = relativeIntervalsForSemitones(semitones)
-  const relativeInterval = randomFromArray(relativeIntervals)
-  const relativeLetter = relativeLetterForInterval(baseNote.letter, relativeInterval)
+  const semitones = semitonesFromNoteToGroup(baseNote, destinationGroup);
+  const relativeIntervals = relativeIntervalsForSemitones(semitones);
+  const relativeInterval = randomFromArray(relativeIntervals);
+  const relativeLetter = relativeLetterForInterval(
+    baseNote.letter,
+    relativeInterval
+  );
 
-  return destinationGroup.find(note => note.letter === relativeLetter)
+  return destinationGroup.find(note => note.letter === relativeLetter);
 }
 
 export default function createExercise(): Exercise {
-  const fromNoteGroup = getRandomNoteGroup()
-  const fromNote = randomFromArray(fromNoteGroup)
+  const fromNoteGroup = getRandomNoteGroup();
+  const fromNote = randomFromArray(fromNoteGroup);
 
-  let toNote
+  let toNote;
 
   do {
     // Because I don't know enough music theory yet so this can fail, and I'm not sure
     // what the correct solution is:
     try {
-      const toNoteGroup = getRandomNoteGroup({ except: fromNote })
-      toNote = validNoteFromGroup(fromNote, toNoteGroup)
+      const toNoteGroup = getRandomNoteGroup({ except: fromNote });
+      toNote = validNoteFromGroup(fromNote, toNoteGroup);
     } catch (e) {
-      continue
+      continue;
     }
-  } while (!toNote)
+  } while (!toNote);
 
-  const distanceBetweenNotes = distance(fromNote, toNote)
+  const distanceBetweenNotes = distance(fromNote, toNote);
 
   return {
     from: fromNote,
     to: toNote,
     distance: distanceBetweenNotes
-  }
+  };
 }
