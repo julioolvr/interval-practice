9cdf67e2a61cfac14ce7a75f76c00ca8f50dc086
Julio Olivera <julio.olvr@gmail.com>
2017-07-08 19:31:20 -0300

    Change exercises button

--- src/components/Exercise.js
+++ src/components/Exercise.js
@@ -1,12 +1,32 @@
 import React from "react";
+
+import Note from "./Note";
 import getRandomInterval from "../lib/exerciseGenerator";
 
-function Exercise() {
-  return (
-    <pre>
-      {JSON.stringify(getRandomInterval(), null, 2)}
-    </pre>
-  );
+class Exercise extends React.Component {
+  state = {
+    interval: getRandomInterval()
+  };
+
+  nextInterval() {
+    this.setState({ interval: getRandomInterval() });
+  }
+
+  render() {
+    const { interval } = this.state;
+
+    return (
+      <div>
+        <div>
+          From: <Note note={interval.from} />
+        </div>
+        <div>
+          To: <Note note={interval.to} />
+        </div>
+        <button onClick={() => this.nextInterval()}>Next</button>
+      </div>
+    );
+  }
 }
 
 export default Exercise;
