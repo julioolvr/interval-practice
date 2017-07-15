e58b0b83711face78510d10bf8a18b2deedda8b9
Julio Olivera <julio.olvr@gmail.com>
2017-07-15 14:22:20 -0300

    Disabled style after a note has been selected

--- src/components/NoteSelector.js
+++ src/components/NoteSelector.js
@@ -4,7 +4,7 @@ import React from "react";
 import "./NoteSelector.css";
 
 import type { Note } from "../lib/notes";
-import { NOTES, noteToString } from "../lib/notes";
+import { NOTES, noteToString, isSameNote } from "../lib/notes";
 
 const NOTE_FOR_STRING = NOTES.reduce((acc, note) => {
   acc[noteToString(note)] = note;
@@ -30,44 +30,21 @@ type Props = {
   onSelect: Note => void
 };
 
-const COLORS = [
-  "#FF1D00",
-  "#FE0724",
-  "#FE0F62",
-  "#FE179E",
-  "#FE1ED5",
-  "#F326FE",
-  "#C32EFE",
-  "#9635FE",
-  "#6D3DFE",
-  "#4845FE",
-  "#4C73FE",
-  "#54A0FD",
-  "#5CCAFD",
-  "#63F0FD",
-  "#6BFDE7",
-  "#72FDC8",
-  "#7AFDAC",
-  "#82FD94",
-  "#93FD89",
-  "#B3FD91",
-  "#D0FD98"
-];
-
 function NoteSelector({ value, onSelect }: Props) {
-  const options = GROUPED_NOTES.map((noteGroup, i) =>
-    <div key={noteGroup[0].letter}>
-      {noteGroup.map((note, j) =>
-        <button
-          key={noteToString(note)}
-          onClick={() => onSelect(note)}
-          className="NoteSelector__note"
-          style={{ backgroundColor: COLORS[i * noteGroup.length + j] }}
-        >
-          {noteToString(note)}
-        </button>
-      )}
-    </div>
+  const options = NOTES.map(note =>
+    <button
+      key={noteToString(note)}
+      onClick={() => onSelect(note)}
+      className={[
+        "NoteSelector__note",
+        value && isSameNote(note, value) && "NoteSelector__note--selected"
+      ]
+        .filter(Boolean)
+        .join(" ")}
+      disabled={!!value}
+    >
+      {noteToString(note)}
+    </button>
   );
 
   return (
